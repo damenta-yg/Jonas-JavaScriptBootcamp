1de7902addcd73f86b0fa7316155c1ee61eece36
damenta-yg <gnitnigdy@outlook.com>
2025-11-17 05:39:40 +0700

    [A] - Section 2 - Fundamental Javascript 1

---
+++ 02-Fundamental-JavaScript-Part1/01-BriefIntroduction.js
@@ -0,0 +1,15 @@
+/*
+Javascript is a high level object oriented multi-pardigm programming language.
+Programming Language -> Instruct computer to do things.
+High-Level -> do not have to worry about complex stuff like memory management.
+Object Oriented -> based on objects for storing most kinds of data.
+Multi Paradigm -> flexible and versatiles of using different styles of programming.
+
+
+Javascript create dynamic effects and web applications in the browser. (Frontend)
+
+Javascript can also create for web applications that run on web servers. (Backend)
+
+*/
+
+alert("GOBLOGGGG!!!!!!!");
