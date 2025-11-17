002621037ac3174fe15e125ade216c3e338c7a3d
damenta-yg <gnitnigdy@outlook.com>
2025-11-18 04:41:34 +0700

    [A] - Section 2 - Coding Exercise 1

---
+++ 02-Fundamental-JavaScript-Part1/02-Operators.js
@@ -0,0 +1,16 @@
+/*
+
+Assignment Operators
+const now = 2037
+
+Mathematical Operators
+using *, + , / , - , %
+
+Typeof operators
+for giving us the data type of 
+
+Comparison Operators to produce boolean value (> , >= <= , ==, ===)
+if x === 3 -> checking is x is equal 3 and data type is number
+
+
+*/
---
+++ 02-Fundamental-JavaScript-Part1/03-CodingExercise1.js
@@ -0,0 +1,16 @@
+let massMark,
+  heightMark,
+  massJohn,
+  heightJohn = 0;
+
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76;
+
+let BMIMark = massMark / (heightMark * heightMark);
+
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark);
+console.log(BMIJohn);
